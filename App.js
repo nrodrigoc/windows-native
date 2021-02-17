f57dd818cf11809b3b648d697d543288e261a0cd
nrodrigoc <nathan--rodrigo@hotmail.com>
2021-02-17 19:03:56 -0300

    rename view

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { NavigationContainer, DrawerActions } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 
 import {
   createDrawerNavigator,
 } from '@react-navigation/drawer';
 
-import TelaDuCarai from './views/TelaDuCarai'
+import Home from './views/Home'
 import SegundaTela from './views/SegundaTela'
 
 const Drawer = createDrawerNavigator();
@@ -16,7 +16,7 @@ function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={TelaDuCarai} />
+        <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="Segunda" component={SegundaTela} />
       </Drawer.Navigator>
     </NavigationContainer>
